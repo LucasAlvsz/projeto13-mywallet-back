371ac69e27441eb650111bd60f83c468cb87cfee
Luana Lessa <lessalsn@gmail.com>
2022-05-20 02:30:19 -0300

    Code review

--- src/usables/getDaysjs.js
+++
@@ -1,9 +0,0 @@
-import dayjs from "dayjs"
-
-export const getDateAndTime = () => {
-	return dayjs().format("HH:mm - DD/MM/YY")
-}
-
-export const getDate = () => {
-	return dayjs().format("DD/MM")
-}
